04e538343220b9d2abadf95671dcc093533b7da7
shunichiYasuda <34476078+shunichiYasuda@users.noreply.github.com>
2018-02-06 17:27:09 +0900

    add dataCleaning() and checkChar()

--- src/application/CorrMatController.java
+++ src/application/CorrMatController.java
@@ -90,7 +90,7 @@ public class CorrMatController {
 				}
 				dataList.add(new CData(tmpData));
 			}
-			
+
 			br.close();
 		} catch (UnsupportedEncodingException e) {
 			// TODO Auto-generated catch block
@@ -113,7 +113,7 @@ public class CorrMatController {
 		for (ComboBox<String> c : colComboArray) {
 			// fieldNameArray と比較して位置を返す
 			int pos = 0;
-			for(String s : fieldNameArray) {
+			for (String s : fieldNameArray) {
 				if (s.equals((String) c.getValue())) {
 					log.appendText("\ncol[" + pos + "]:\t" + (String) c.getValue());
 					vPosList.add(pos);
@@ -121,12 +121,12 @@ public class CorrMatController {
 				}
 				pos++;
 			}
-		}// end of 列変数のセット
-		//行変数名のセットが必要
+		} // end of 列変数のセット
+			// 行変数名のセットが必要
 		List<Integer> hPosList = new ArrayList<Integer>();
-		for(ComboBox<String> c: rowComboArray) {
+		for (ComboBox<String> c : rowComboArray) {
 			int pos = 0;
-			for(String s: fieldNameArray) {
+			for (String s : fieldNameArray) {
 				if (s.equals((String) c.getValue())) {
 					log.appendText("\nrow[" + pos + "]:\t" + (String) c.getValue());
 					hPosList.add(pos);
@@ -135,9 +135,66 @@ public class CorrMatController {
 				pos++;
 			}
 		}
-		
-		
-	}//end of execAction()
+		// ここまでで、vPosList と hPosList に対象となるフィールド番号が入っている。
+		// 次に、vPosList に入っている番号のそれぞれに hPosList に入っている番号を順番に対応させる。
+		// とりあえず、ペアを確認してみる
+		int vPos, hPos;
+		for (Integer v : vPosList) {
+			vPos = v.intValue();
+			for (Integer h : hPosList) {
+				hPos = h.intValue();
+				System.out.println("(" + vPos + "," + hPos + ")");
+				// ペアになった番号から、そのフィールドのデータを読み込むのだが、どちらかのデータが空白であったり、
+				// 排除コードであったりした場合にデータを「つめる」必要がある。
+				// これはかなりやっかいなので別メソッドにする
+				dataCleaning(vPos, hPos);
+			}
+		}
+		//
+	}// end of execAction()
+
+	// データクリーニング
+	private void dataCleaning(int v, int h) {
+		// dataList を壊さないようにデータを外に取り出す。
+		String[] colOriginalDataStrArray = dataList.get(v).get();
+		String[] rowOriginalDataStrArray = dataList.get(h).get();
+		//本当はTextField から除外番号を読み取るが、いまはここで与える
+		String[] eliminate = {"99","9",""};
+		//冗長にはなるが、いったん排除文字を含めて、長さを合わせておく。
+		int arraySize = colOriginalDataStrArray.length;
+		if(rowOriginalDataStrArray.length < arraySize) {
+			arraySize = rowOriginalDataStrArray.length;
+		}
+		//短い方にあわせて、いったん配列を作っておく
+		String[] colTmpDataArray = new String[arraySize];
+		String[] rowTmpDataArray = new String[arraySize];
+		//データを頭から詰める。つまり長さが合わなければ短い方に合わせる
+		for(int i=0;i<arraySize;i++) {
+			colTmpDataArray[i] = colOriginalDataStrArray[i];
+			rowTmpDataArray[i] = rowOriginalDataStrArray[i];
+		}
+		//両方を頭からチェックしていき排除文字があったらつめるのだけれど
+		//どちらかにあれば、両方のその場所を詰めなければならないので配列で扱うには
+		//やっかいなので、Listを使う
+		// check
+		for (String s : colOriginalDataStrArray) {
+			//文字をチェック
+			if (checkChar(s,eliminate)) {
+				System.out.print("空白");
+			} else {
+				System.out.print("\t" + s);
+			}
+		}
+		System.out.println();
+	}
+	//除外文字かどうかの判断。除外文字なら true
+	private boolean checkChar(String in, String[] e) {
+		boolean r = false;
+		for(String s: e) {
+			if(in.equals(s)) r = true;
+		}
+		return r;
+	}
 
 	@FXML
 	private void saveAction() {
